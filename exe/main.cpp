335fada2f9791eeb48d9e8ee3ad7f5e3549f7f22
Fangwen Shu <shu@pc-2165.kl.dfki.de>
2019-06-30 22:57:12 +0200

    updated

--- exe/main.cpp
+++ exe/main.cpp
@@ -5,7 +5,10 @@ int main() {
     std::cout << "MAIN FUNCTION RUNING" << std::endl;
 
     // BinFile_reader test;
-    vins_BinFile_reader::testFunc();
+    // vins_PoseGraph_reader::testFunc();
+    vins_PoseGraph_reader::loadPoseGraph_from_vins();
+
+
 
     return 0;
 }
--- src/read_binFile.h
+++ src/read_binFile.h
@@ -7,24 +7,28 @@
 // using namespace cv;
 
 
-namespace vins_BinFile_reader {
+namespace vins_PoseGraph_reader {
     // This namespace define the functions used to read pose graph
     // generated from VINS-Mono, MYNT EYE supported
     void testFunc();
+    void loadPoseGraph_from_vins();
+    void saveImage_txt_in_COLMAP_format();
+    void saveCamera_txt_in_COLMAP_format();
+    void savePoints3D_txt_in_COLMAP_format();
 }
 
-class BinFile_reader {
-    public:
-        BinFile_reader();                                                  // save_file_path{save_file_path_} {}
-        ~BinFile_reader();
+// class BinFile_reader {
+//     public:
+//         BinFile_reader();                                                  // save_file_path{save_file_path_} {}
+//         ~BinFile_reader();
 
-        // custom member function
-        void testFunc();                                                                                    
-        // bool reader();
-        // bool saver();
+//         // custom member function
+//         void testFunc();                                                                                    
+//         // bool reader();
+//         // bool saver();
 
-    private:
-        std::string input_file_path = "";
-        std::string save_file_path = "";
-};
+//     private:
+//         std::string input_file_path = "";
+//         std::string save_file_path = "";
+// };
 

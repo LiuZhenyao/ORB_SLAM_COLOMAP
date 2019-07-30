a91c7ee4e066f11a8152b1e5a73132b4097d4804
PeterFWS <sfw811@hotmail.com>
2019-07-30 12:25:35 +0200

    updated

---
+++ utility_python/trajectory_analysis_ORBSLAM.py
@@ -0,0 +1,8 @@
+import numpy as np
+from numpy import pi
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import gps_data_analysis
+from numpy.linalg import inv
+import quaternion
+
